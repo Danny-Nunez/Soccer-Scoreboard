12d3bbeadac01a136f8823199aa9e2274ea205e6
Danny Nunez <114783191+dnunez22@users.noreply.github.com>
2022-10-03 21:11:22 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+let num1 = 0
+let pointScreen = document.getElementById("point-El")
+let num2 = 0
+let point2Screen = document.getElementById("point2-El")
+
+
+function addOne(){
+    num1 += 1
+    pointScreen.textContent = num1
+}
+
+function addTwo(){
+    num1 += 2
+    pointScreen.textContent = num1
+}
+function addThree(){
+    num1 += 3
+    pointScreen.textContent = num1
+}
+
+function add2One(){
+    num2 += 1
+    point2Screen.textContent = num2
+}
+
+function add2Two(){
+    num2 += 2
+    point2Screen.textContent = num2
+}
+function add2Three(){
+    num2 += 3
+    point2Screen.textContent = num2
+}
+
+
+    
